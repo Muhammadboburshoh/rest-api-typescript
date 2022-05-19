192bce336cdc480531c9fdec27e9e586e0ac48c5
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-05-19 17:52:13 +0500

    Add user model

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 import config from 'config';
+import logger from "./logger"
 
 async function connect() {
   const dbUrl = config.get<string>('dbUrl');
 
   try {
     await mongoose.connect(dbUrl);
-    console.log('Db connected');
+    logger.info('Db connected');
   } catch (error) {
-    console.error('Could not found to db', error);
+    logger.error('Could not found to db', error);
     process.exit(1);
   }
 }
